1593de4db4464ef30ee9cb4272306e78768f62ea
lili <2355308940@qq.com>
2018-05-03 11:33:07 +0800

    log测试

--- Pollution2/app/src/main/java/com/tim/pollution/MainActivity.java
+++ Pollution2/app/src/main/java/com/tim/pollution/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
@@ -55,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        Log.e("lili","---------");
         fragments = new ArrayList<>();
         fragments.add(new FirstPageFragment());
         fragments.add(new MapFragment());
